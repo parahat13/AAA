1748313af78d824980028fc07f09abb151a606a6
Home <Home@10.0.0.74>
2018-03-14 22:26:20 -0500

    test commit

---
+++ src/List/test/ArrayListExample.java
@@ -0,0 +1,33 @@
+package List.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListExample {
+	
+	public static void main(String[] args) {
+		
+		List<String> list= new ArrayList<>();
+		list.add("water");
+		list.add("coffee");
+		list.add("bottle");
+		list.add("cup");
+		
+		System.out.println(list.indexOf("coffee"));
+		
+		
+		String[] array=list.toArray(new String[4]);
+		Arrays.sort(array);
+		System.out.println(Arrays.binarySearch(array, "bottle"));
+		
+		List<List<Integer>> lis= new ArrayList<>();
+		
+		list.clear();
+		
+		System.out.println(list);
+		
+		
+	}
+
+}
