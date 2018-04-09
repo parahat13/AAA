ad963f82a8c6e9e60eabfa107d7a70c9e175ceaf
Home <Home@10.0.0.75>
2018-04-08 20:03:42 -0500

    excel data

---
+++ src/ExcelData/ExcelTest.java
@@ -0,0 +1,25 @@
+package ExcelData;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ExcelTest {
+	
+	
+	public static void main(String[] args) throws IOException {
+
+		ExcelUtilClass.openExcelFile("C:\\Users\\Home\\eclipse-workspace\\AAA\\dataFiles\\ExcelData.xlsx", "data");
+		
+		
+		List<String> data = new ArrayList<String>();
+
+		for (int i = 0; i <4; i++) {
+			data.add(ExcelUtilClass.getCellData(0, i));
+			System.out.print(data.get(i)+"\t");
+		}
+
+	}
+
+}
